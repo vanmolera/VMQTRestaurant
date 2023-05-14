4775531f51a30e20e9a7b3967e008f51ce59f845
Van <120751665+vanmolera@users.noreply.github.com>
2023-05-14 22:26:03 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// // preload - loading will end after doc loaded
+
+
+window.addEventListener("load", function () {
+
+    const preloader = document.querySelector("[data-preaload]");
+    preloader.style.display = "none";
+
+});
